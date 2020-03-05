42b4daaaed86d1700126709a6b8fefbbf2227ae1
SashaBondarchuk <o.bondarchuk9962@gmail.com>
2020-03-05 18:36:38 +0200

    05.03(2)

---
+++ demo05/routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+// router.get('/', function(req, res) {
+//     res.render('index', {
+//         title: 'My Demo Prog'
+//     });
+// });
+router.post('/', function(req, res) {
+    if (isNaN(req.body.firstvalue && req.body.firstvalue) === true) {
+        res.render('Not a Number!');
+    } else {
+        res.render('index', {
+            title: 'calculator:',
+            Result: 'multiplication = ' + req.body.firstvalue * req.body.secondvalue + ' ' +
+                'Sum = ' + parseInt(parseInt(req.body.firstvalue) + parseInt(req.body.firstvalue)) + ' ' +
+                'divide = ' + (req.body.firstvalue / req.body.secondvalue)
+        });
+    }
+});
+
+module.exports = router;
